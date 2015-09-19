551ce1e510b5f88b2577f434e3c7be6e37d178c6
atijust <tsujita.shou@gmail.com>
2015-09-20 04:40:29 +0900

    Remove redundant parameter from Blade::make()

--- src/Blade.php
+++ src/Blade.php
@@ -2,21 +2,18 @@
 namespace Ronin;
 
 use Illuminate\Contracts\Container\Container as ContainerContract;
-use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 
 class Blade
 {
     /**
      * @param string|string[] $viewPaths
      * @param string $cachePath
-     * @param \Illuminate\Contracts\Events\Dispatcher $dispatcher
      * @param \Illuminate\Contracts\Container\Container $container
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\Factory
      */
     public static function make(
         $viewPaths,
         $cachePath,
-        DispatcherContract $dispatcher = null,
         ContainerContract $container = null
     ) {
         $blade = new Container();
@@ -24,10 +21,6 @@ class Blade
         $blade['view.paths'] = is_array($viewPaths) ? $viewPaths : [$viewPaths];
 
         $blade['view.compiled'] = $cachePath;
-
-        if ($dispatcher !== null) {
-            $blade->instance('events', $dispatcher);
-        }
 
         if ($container !== null) {
             $blade->instance('container', $container);
--- tests/BladeTest.php
+++ tests/BladeTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Container\Container;
-use Illuminate\Events\Dispatcher;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\View\Compilers\BladeCompiler;
 use Ronin\Blade;
@@ -16,23 +15,13 @@ class BladeTest extends PHPUnit_Framework_TestCase
             (new BladeCompiler(new Filesystem(), '/cache'))->getCompiledPath('test'),
             $blade->getEngineResolver()->resolve('blade')->getCompiler()->getCompiledPath('test')
         );
-        $this->assertInstanceOf('\Illuminate\Contracts\Events\Dispatcher', $blade->getDispatcher());
-        $this->assertInstanceOf('\Illuminate\Contracts\Container\Container', $blade->getContainer());
-    }
-
-    public function testMakeWithDispatcher()
-    {
-        $dispatcher = new Dispatcher();
-
-        $blade = Blade::make('/view', '/cache', $dispatcher);
-        $this->assertSame($dispatcher, $blade->getDispatcher());
     }
 
     public function testMakeWithContainer()
     {
         $container = new Container();
 
-        $blade = Blade::make('/view', '/cache', null, $container);
+        $blade = Blade::make('/view', '/cache', $container);
         $this->assertSame($container, $blade->getContainer());
     }
 
